dcc05cc2935c238c7534e290d8cdcccc5ffeb095
DanielJamesK <danieljkurz@gmail.com>
2020-12-01 18:14:09 +1000

    Added functionality to the btnHold element that adds the current score to the players total score, checks to see whether the player has won, and if they haven't switch to the next players turn. Also added functionality to the btnNew element that refreshes the game by calling the init function

--- script.js
+++ script.js
@@ -43,7 +43,49 @@ const switchPlayer = function () {
   player1El.classList.toggle('player--active')
 }
 // Rolling dice functionality
+btnRoll.addEventListener('click', function () {
+  if (playing) {
 
+    // 1. Generating a random dice roll
+    const dice = Math.trunc(Math.random() * 6) + 1
+
+    // 2. Display dice
+    diceEl.classList.remove('hidden')
+    diceEl.src = `dice-${dice}.png`
+
+    // 3. Check for rolled 1
+    if (dice !== 1) {
+      // Add dice to current score
+      currentScore += dice
+      document.getElementById(`current--${activePlayer}`).textContent = currentScore
+
+    } else {
+      // Switch to next player
+      switchPlayer()
+    }
+  }
+})
+
+btnHold.addEventListener('click', function () {
+  if (playing) {
+    // 1. Add current score to active player's score
+    scores[activePlayer] += currentScore
+    document.getElementById(`score--${activePlayer}`).textContent = scores[activePlayer]
+    // 2. Check if player's score is >= 100
+    if (scores[activePlayer] >= 20) {
+      // Finish the game
+      playing = false
+      diceEl.classList.add('hidden')
+      document.querySelector(`.player--${activePlayer}`).classList.add('player--winner')
+      document.querySelector(`.player--${activePlayer}`).classList.remove('player--active')
+    } else {
+      // Switch to the next player
+      switchPlayer()
+    }
+  }
+})
+
+btnNew.addEventListener('click', init)
 
 
 
