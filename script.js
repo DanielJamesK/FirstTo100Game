6248904480ab6429f06fa08ac785e27a4ffa5efc
DanielJamesK <danieljkurz@gmail.com>
2020-12-06 15:49:07 +1000

    Changed game title and set win amount to 100

--- script.js
+++ script.js
@@ -11,6 +11,12 @@ const diceEl = document.querySelector('.dice')
 const btnNew = document.querySelector('.btn--new')
 const btnRoll = document.querySelector('.btn--roll')
 const btnHold = document.querySelector('.btn--hold')
+const currentBg1 = document.getElementById('current-bg-1')
+const currentBg2 = document.getElementById('current-bg-2')
+const btnBgChange1 = document.getElementById('change-bg-1')
+const btnBgChange2 = document.getElementById('change-bg-2')
+const btnBgChange3 = document.getElementById('change-bg-3')
+const btnBgChange4 = document.getElementById('change-bg-4')
 
 let scores, currentScore, activePlayer, playing
 
@@ -34,6 +40,44 @@ const init = function () {
 }
 
 init()
+
+btnBgChange1.addEventListener('click', function bgChange1() {
+  document.querySelector('body').style.backgroundImage = 'linear-gradient(to top left, #120249 0%, #8044f0 100%)'
+  currentBg1.style.backgroundColor = '#120249'
+  currentBg2.style.backgroundColor = '#120249'
+  score0El.style.color = '#120249'
+  score1El.style.color = '#120249'
+  document.querySelector('.name1').style.color = '#120249'
+  document.querySelector('.name2').style.color = '#120249'
+
+})
+btnBgChange2.addEventListener('click', function bgChange2() {
+  document.querySelector('body').style.backgroundImage = 'linear-gradient(to top left, #012e15 0%, #45fcc5 100%)'
+  currentBg1.style.backgroundColor = '#012e15'
+  currentBg2.style.backgroundColor = '#012e15'
+  score0El.style.color = '#012e15'
+  score1El.style.color = '#012e15'
+  document.querySelector('.name1').style.color = '#012e15'
+  document.querySelector('.name2').style.color = '#012e15'
+})
+btnBgChange3.addEventListener('click', function bgChange3() {
+  document.querySelector('body').style.backgroundImage = 'linear-gradient(to top left, #031949 0%, #0973fd 100%'
+  currentBg1.style.backgroundColor = '#031949'
+  currentBg2.style.backgroundColor = '#031949'
+  score0El.style.color = '#031949'
+  score1El.style.color = '#031949'
+  document.querySelector('.name1').style.color = '#031949'
+  document.querySelector('.name2').style.color = '#031949'
+})
+btnBgChange4.addEventListener('click', function bgChange4() {
+  document.querySelector('body').style.backgroundImage = 'linear-gradient(to top left, #817903 0%, #f0fd3b 100%)'
+  currentBg1.style.backgroundColor = '#817903'
+  currentBg2.style.backgroundColor = '#817903'
+  score0El.style.color = '#817903'
+  score1El.style.color = '#817903'
+  document.querySelector('.name1').style.color = '#817903'
+  document.querySelector('.name2').style.color = '#817903'
+})
 
 const switchPlayer = function () {
   document.getElementById(`current--${activePlayer}`).textContent = 0
